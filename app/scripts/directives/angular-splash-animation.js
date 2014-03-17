2fdd33a3c6b4bac42afabec5a38f2d0f544e844c
slopjong <git@slopjong.de>
2014-03-17 19:30:13 +0100

    Some initial implementation of the directive

--- app/scripts/directives/angular-splash-animation.js
+++ app/scripts/directives/angular-splash-animation.js
@@ -2,14 +2,40 @@
 
 angular
   .module('sj.splash', [])
-  .directive("splashAnimation", function() {
+  .value('splashAnimationConfig', {
+    currentSplashIndex: 0,
+    changeInterval: 3e3
+  })
+  .directive("splashAnimation", ['splashAnimationConfig', '$timeout', function(splashAnimationConfig, $timeout) {
+
     return {
       restrict: 'E',
-      template: '<div>test</div>',
+      template: '<div class="sa-container"><div ng-transclude></div><div class="sa-hidden" ng-transclude></div></div>',
       replace: true,
+      transclude: true,
       link: function(scope, element, attrs) {
+
+        // parse the words (= string) into a nested javascript array
+        if (angular.isDefined(attrs.words)) {
+          scope.words = scope.$eval(attrs.words);
+        }
+
+        if (angular.isDefined(attrs.interval)) {
+          scope.changeInterval = scope.$eval(attrs.interval);
+        } else {
+          scope.changeInterval = splashAnimationConfig.changeInterval;
+        }
+
+        // make the slogan parts invisible and put the absolute
+        // positioned elements there
+        scope.sloganParts = element.find('.sa-word');
+        angular.forEach(scope.sloganParts, function(part) {
+          //angular.element(part).addClass('sa-hidden');
+          // create new dom elements here by using a service which
+          // compiles a template
+        });
       }
     };
-  })
+  }])
 
   ;
