2c9f61e6250183b0ae37e09c4db74cea015bcf16
slopjong <git@slopjong.de>
2014-06-25 13:32:07 +0200

    Updates

---
+++ scripts/9a547db4.scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("splashAnimationApp",["sj.splash"]),angular.module("splashAnimationApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
